ed94a2f1400518c355bcadf9dbd97de2f471fd0f
CauanLagrotta <cauanlagrotta.dev@gmail.com>
2025-07-11 11:16:37 -0300

    fix: consertando o scroll vertical

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,22 +1,25 @@
 import {
   View,
-  Image,
-  ScrollView,
-  ActivityIndicator,
   Text,
+  ActivityIndicator,
+  ScrollView,
+  Image,
   FlatList,
 } from "react-native";
-import { images } from "@/constants/images";
-import { icons } from "@/constants/icons";
-import SearchBar from "@/components/SearchBar";
 import { useRouter } from "expo-router";
+
 import useFetch from "@/services/useFetch";
 import { fetchMovies } from "@/services/api";
-import MovieCard from "@/components/MovieCard";
 import { getTrendingMovies } from "@/services/appwrite";
+
+import { icons } from "@/constants/icons";
+import { images } from "@/constants/images";
+
+import SearchBar from "@/components/SearchBar";
+import MovieCard from "@/components/MovieCard";
 import TrendingCard from "@/components/TrendingCard";
 
-export default function Index() {
+const Index = () => {
   const router = useRouter();
 
   const {
@@ -33,9 +36,17 @@ export default function Index() {
 
   return (
     <View className="flex-1 bg-primary">
-      <Image source={images.bg} className="absolute w-full z-0" />
+      <Image
+        source={images.bg}
+        className="absolute w-full z-0"
+        resizeMode="cover"
+      />
 
-      <View className="flex-1 px-5">
+      <ScrollView
+        className="flex-1 px-5"
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ minHeight: "100%", paddingBottom: 10 }}
+      >
         <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
 
         {moviesLoading || trendingLoading ? (
@@ -47,44 +58,44 @@ export default function Index() {
         ) : moviesError || trendingError ? (
           <Text>Error: {moviesError?.message || trendingError?.message}</Text>
         ) : (
-          <View className="flex-1 mb-5">
+          <View className="flex-1 mt-5">
             <SearchBar
-              onPress={() => router.push("/search")}
+              onPress={() => {
+                router.push("/search");
+              }}
               placeholder="Search for a movie"
             />
 
-            {trendingMovies?.length && (
+            {trendingMovies && (
               <View className="mt-10">
                 <Text className="text-lg text-white font-bold mb-3">
                   Trending Movies
                 </Text>
-
                 <FlatList
                   horizontal
                   showsHorizontalScrollIndicator={false}
-                  ItemSeparatorComponent={() => <View className="w-4" />}
                   className="mb-4 mt-3"
                   data={trendingMovies}
+                  contentContainerStyle={{
+                    gap: 26,
+                  }}
                   renderItem={({ item, index }) => (
                     <TrendingCard movie={item} index={index} />
                   )}
                   keyExtractor={(item) => item.movie_id.toString()}
+                  ItemSeparatorComponent={() => <View className="w-4" />}
                 />
               </View>
             )}
 
             <>
               <Text className="text-lg text-white font-bold mt-5 mb-3">
-                Latest movies
+                Latest Movies
               </Text>
 
               <FlatList
                 data={movies}
-                renderItem={({ item }) => (
-                  <>
-                    <MovieCard {...item} />
-                  </>
-                )}
+                renderItem={({ item }) => <MovieCard {...item} />}
                 keyExtractor={(item) => item.id.toString()}
                 numColumns={3}
                 columnWrapperStyle={{
@@ -94,14 +105,14 @@ export default function Index() {
                   marginBottom: 10,
                 }}
                 className="mt-2 pb-32"
-                scrollEnabled={true}
-                contentContainerStyle={{ paddingBottom: 20, flexGrow: 1 }}
-                showsVerticalScrollIndicator={false}
+                scrollEnabled={false}
               />
             </>
           </View>
         )}
-      </View>
+      </ScrollView>
     </View>
   );
-}
+};
+
+export default Index;
